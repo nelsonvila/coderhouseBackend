2a4023b869092a0409e237f8972f8c223a21aac7
nelsonvila <nelson_vila@live.com.ar>
2023-04-25 18:57:14 -0300

    43. Documentación de APIs

---
+++ clase43/ejercicio1/myapp/app.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var productsRouter = require('./routes/products');
+
+
+var app = express();
+//view engine
+
+app.set('views', path.join(__dirname), 'views')
+app.set('view engine', 'hbs')
+
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/api/productos', productsRouter);
+
+module.exports = app;
